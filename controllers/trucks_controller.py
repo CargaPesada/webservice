b229b897f80f43743cf302bfa48aa70b52bc365b
Vitor <vitorjuliani99@gmail.com>
2019-09-26 10:53:15 -0300

    trucks endpoints

--- controllers/trucks_controller.py
+++ controllers/trucks_controller.py
@@ -1,17 +1,17 @@
 from flask import request
-from flask_restful import Resource, reqparse, abort
-from database.interface import FirebaseInterface
+from flask_restful import reqparse, Resource
 import json
 
+from database.interface import FirebaseInterface
 
-class OfficesController(Resource):
 
+class TrucksController(Resource):
     def __init__(self):
         self.parser = reqparse.RequestParser()
         self.interface = FirebaseInterface()
 
     def get(self):
-        dic = {"data": self.interface.getData("offices")}
+        dic = {"data": self.interface.getData("trucks")}
 
         data = json.dumps(dic)
         data_json = json.loads(data)
@@ -19,38 +19,36 @@ class OfficesController(Resource):
 
     def post(self):
         result = request.get_json()
-        self.interface.addData(result, "offices", None)
+        self.interface.addData(result, "trucks", None)
 
 
-class OfficeControllerById(Resource):
-
+class TrucksControllerById(Resource):
     def __init__(self):
         self.parser = reqparse.RequestParser()
         self.interface = FirebaseInterface()
 
-    def get(self, office_id):
-        dic = {"data": self.interface.getData("offices", office_id)}
+    def get(self, truck_id):
+        dic = {"data": self.interface.getData("trucks", truck_id)}
 
         data = json.dumps(dic)
         data_json = json.loads(data)
         return data_json
 
-    def delete(self, office_id):
-        self.interface.deleteData("offices", office_id)
+    def delete(self, truck_id):
+        self.interface.deleteData("trucks", truck_id)
 
-    def put(self, office_id):
+    def put(self, truck_id):
         result = request.get_json()
-        self.interface.addData(result, "offices", office_id)
+        self.interface.addData(result, "trucks", truck_id)
 
 
-class OfficeControllerByRegion(Resource):
-
+class TrucksControllerByRegion(Resource):
     def __init__(self):
         self.parser = reqparse.RequestParser()
         self.interface = FirebaseInterface()
 
     def get(self, region):
-        dic = {"data": self.interface.getDataByField("offices", 'pais', region)}
+        dic = {"data": self.interface.getDataByField("trucks", 'pais', region)}
 
         data = json.dumps(dic)
         data_json = json.loads(data)
