8498a333d2aa095729cd610f3d4fdc367b2a76e2
Vitor <vitorjuliani99@gmail.com>
2019-11-04 23:12:58 -0300

    crud serviços

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
 import firebase_admin
 from flask import Flask
 from flask_restful import Api
-from controllers import users_controller, trucks_controller, offices_controller, schedules_controller
+from controllers import users_controller, trucks_controller, offices_controller, schedules_controller, \
+    services_controller
 from firebase_admin import credentials
 from flask_cors import CORS
 
@@ -42,6 +43,10 @@ api.add_resource(trucks_controller.TrucksControllerByRegion,
 # Schedules endpoints
 api.add_resource(schedules_controller.SchedulesController,
                  '/schedule/register', '/schedule/delete/<office_id>/<event_id>', '/schedule/list/<office_id>')
+
+# Services endpoints
+api.add_resource(services_controller.ServicesController,
+                 '/service/list', '/service/register', '/service/delete/<service>', '/service/update')
 
 if __name__ == '__main__':
     # This is used when running locally only. When deploying to Google App
