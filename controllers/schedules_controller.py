d12b48726d56dbdaeb6b2904eebfa0a216dd64fd
Vitor <vitorjuliani99@gmail.com>
2019-11-22 11:09:19 -0300

    insert put method and refactor code

--- controllers/schedules_controller.py
+++ controllers/schedules_controller.py
@@ -22,31 +22,12 @@ class SchedulesController(Resource):
                 event_id = self.interface.getData("const_data", "office_id")
                 schedule.id = int(event_id["id"]) + 1
 
-            office = self.interface.getData("offices", schedule.oficina)
+            office = self.validateOfficeAndUpdate(schedule)
 
-            if office is None:
-                raise Exception("Oficina informada não encontrada")
-            else:
-                office["agenda"].append(str(schedule.id))
+            self.validateDate(schedule)
+            self.validateUser(schedule.mecanico)
 
-            schedule_date = self.interface.getDataByTwoFields("schedules", "data", schedule.data, "oficina", schedule.oficina)
-
-            if schedule_date:
-                raise Exception("Esta oficina já possui evento para essa data")
-
-            user = self.interface.getData("users", schedule.mecanico)
-
-            if user is None:
-                raise Exception("Mecânico não encontrado")
-            elif user["cargo"] != "mecanico":
-                raise Exception("Usuário fornecido possui o cargo de " + user["cargo"] + " e não o de mecânico")
-
-            truck = self.interface.getDataByField("trucks", "placa", schedule.caminhao)
-
-            if truck is None:
-                raise Exception("Nenhum caminhão foi encontrado com essa placa")
-            else:
-                schedule.caminhao = truck[0]["id"]
+            schedule = self.validateTruckAndUpdate(schedule)
 
             self.interface.setData(schedule.__dict__, "schedules", str(schedule.id))
             self.interface.updateData(office, "offices", schedule.oficina)
@@ -80,6 +61,43 @@ class SchedulesController(Resource):
 
         return result, http_return_code
 
+    def put(self):
+        req = request.get_json()
+
+        try:
+            self.validateRequest(req)
+
+            schedule = Schedule(req)
+
+            if schedule.id is None:
+                raise Exception("O campo id deve ser enviado")
+            else:
+                previous_schedule = self.interface.getData("schedules", schedule.id)
+
+            if schedule.data != previous_schedule["data"] and schedule.oficina == previous_schedule["oficina"]:
+                self.validateDate(schedule)
+
+            self.validateUser(schedule.mecanico)
+
+            schedule = self.validateTruckAndUpdate(schedule)
+
+            if previous_schedule["oficina"] != schedule.oficina:
+                self.interface.deleteItemFromArray("offices", previous_schedule["oficina"], "agenda", schedule.id)
+
+                office = self.validateOfficeAndUpdate(schedule)
+                self.interface.updateData(office, "offices", schedule.oficina)
+
+            self.interface.updateData(schedule.__dict__, "schedules", str(schedule.id))
+
+            result = "Evento atualizado com sucesso"
+            http_return_code = 201
+
+        except Exception as e:
+            result = str(e)
+            http_return_code = 400
+
+        return result, http_return_code
+
     @staticmethod
     def validateRequest(req):
         expected_request = {"titulo",
@@ -98,3 +116,36 @@ class SchedulesController(Resource):
             raise Exception("O campo " + keys_not_found[0] + " deve ser enviado")
         elif keys_not_found.__len__() > 1:
             raise Exception("Os seguintes campos estão faltando: " + ", ".join(keys_not_found))
+
+    def validateOfficeAndUpdate(self, schedule):
+        office = self.interface.getData("offices", schedule.oficina)
+
+        if office is None:
+            raise Exception("Oficina informada não encontrada")
+        else:
+            office["agenda"].append(str(schedule.id))
+            return office
+
+    def validateDate(self, schedule):
+        schedule_date = self.interface.getDataByTwoFields("schedules", "data", schedule.data, "oficina",
+                                                          schedule.oficina)
+
+        if schedule_date:
+            raise Exception("Esta oficina já possui evento para essa data")
+
+    def validateUser(self, mecanico):
+        user = self.interface.getData("users", mecanico)
+
+        if user is None:
+            raise Exception("Mecânico não encontrado")
+        elif user["cargo"] != "mecanico":
+            raise Exception("Usuário fornecido possui o cargo de " + user["cargo"] + " e não o de mecânico")
+
+    def validateTruckAndUpdate(self, schedule):
+        truck = self.interface.getDataByField("trucks", "placa", schedule.caminhao)
+
+        if truck is None:
+            raise Exception("Nenhum caminhão foi encontrado com essa placa")
+        else:
+            schedule.caminhao = truck[0]["id"]
+            return schedule
